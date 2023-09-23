f46066a1955cf4b1b1be9cb4230ccb40ff0d87f1
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-09-23 11:06:09 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <FirstPost> Hello this is current Date: </FirstPost> 
+        <FirstPost> Ciao this is current Date: </FirstPost> 
 
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
