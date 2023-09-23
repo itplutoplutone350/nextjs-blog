dc0011dd4a32478ab3b64a466ca03d96ee83af33
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-09-23 21:40:36 +0200

    Update index.js
    Add button with likes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 import FirstPost from './posts/first-post';
-import LikeButton from './posts/first-post';
+import {LikeButton} from './posts/first-post';
 
 
 export default function Home() {
