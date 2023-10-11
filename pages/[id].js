cc534639d689edd0ca3ddfbf37a7d7908e27f62c
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-10-11 16:49:45 +0200

    Update [id].js

--- pages/[id].js
+++ pages/[id].js
@@ -1,1 +1,15 @@
-[
+
+
+export async function getStaticProps({ params }) {
+  const 
+  return {
+    props: {
+      params.id,
+    },
+  };
+}
+
+
+export default function Post(props) {
+  return <p>{props}...</p>;
+}
