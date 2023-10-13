9d34f5857d394b7f54c1c9bf12812b6a6dafbc54
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-10-13 19:43:13 +0200

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,9 @@
 import { Wallet } from '@near-wallet-selector/my-near-wallet';
+const CONTRACT_ADDRESS = process.env.CONTRACT_NAME;
+
+// When creating the wallet you can optionally ask to create an access key
+// Having the key enables to call non-payable methods without interrupting the user to sign
+const wallet = new Wallet({ createAccessKeyFor: CONTRACT_ADDRESS })
 
 let isSignedIn =  wallet.startUp();
 // funzione di generazione array paths
