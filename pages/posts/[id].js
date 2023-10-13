a33ad84e375d2607c5f375988cf3f99b146c16c4
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-10-13 20:54:13 +0200

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,12 +1,12 @@
 import { setupWalletSelector } from "@near-wallet-selector/core";
 import { setupModal } from "@near-wallet-selector/modal-ui";
-import { setupNearWallet } from "@near-wallet-selector/near-wallet";
-/*
+import { setupNearWallet } from "@near-wallet-selector/my-near-wallet";
+
 const selector = await setupWalletSelector({
   network: "testnet",
   modules: [setupNearWallet()],
 });
-
+/*
 const modal = setupModal(selector, {
   contractId: "test.testnet",
 });
