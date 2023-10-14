92c5e13296c47aedcef92fb75b549f5a2aa7ad78
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-10-14 08:27:43 +0200

    Update [id].js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -12,7 +12,7 @@ if (typeof window !== 'undefined') {
  // My NEAR Wallet for Wallet Selector can be setup without any params or it can take few optional params, see options below.
 const myNearWallet = setupMyNearWallet({
   walletUrl: "https://testnet.mynearwallet.com",
-  iconUrl: "../../public/Mynearwallet.ico" // optional
+  iconUrl: "../../public/favicon.ico" // optional
 });  
   const selector = await setupWalletSelector({
   network: "testnet",
