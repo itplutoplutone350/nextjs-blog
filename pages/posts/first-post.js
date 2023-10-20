4c0aacb0111109ae12b3bf30bde7e75a4497cfcf
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-10-20 20:44:29 +0200

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -31,12 +31,14 @@ export default function FirstPost({ children }) {
       <p>  </p>
       <p>  </p>
       <p>  </p>
-      <p><Link className={styles.card} href="../index">Go to Message editor</Link></p>
+      <p><Link className={styles.cardred} href="../index">Go to Message editor</Link></p>
      <p>  </p> 
      <p>  </p>
       <p>  </p>
       <p>  </p>
-      <p><Link className={styles.card} href="https://test.near.org/embed/plutoplutone347.testnet/widget/MsgManager-1">Go to NEAR BOS Message editor</Link> </p>
+    <p>  </p>
+      <p>  </p>
+      <p><Link className={styles.cardred} href="https://test.near.org/embed/plutoplutone347.testnet/widget/MsgManager-1">Go to NEAR BOS Message editor</Link> </p>
       </h5>
     </div>
   );
