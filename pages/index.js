d2e6464872eccae25a64cf752de3728664c1a9f5
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-10-22 08:42:54 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -152,8 +152,8 @@ const gestisciInputChangeAddMessage = (e) => {
 
  const linktomsg = "https://messagetotheworld.vercel.app/posts/" + (lastmsg-1).toString(); 
  //esempio di unixdata = 1695188948769211503;
- const unixdata = Date.now()  / 1000000000 ;
- const difftime = unixdata - message.data;
+ const unixdata = Date.now();
+ const difftime = unixdata - (message.data /1000000000);
  return (
     
     <div className={styles.container}>
