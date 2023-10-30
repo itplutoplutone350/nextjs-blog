12b3bcf12ab17dab99ae2488ebaf147c899166ba
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-10-30 20:31:46 +0100

    Update first-post.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -126,8 +126,8 @@ export function DropdownMenuMsg({ children, options = [{}], selectedOption, onOp
       <select value={selectedOption} onChange={handleOptionChange}>
         {options.map((option, index) => 
           (option.sender === userid) ? (
-            <option key={index} value={option.text} label={option.text} title={option.text}  >
-              {option.text}
+            <option key={index} value={index} label={option.text} title={option.text}  >
+              {index}
             </option>
           ) : (
             <></>
