5c546db543e682286d3bfa7c26f7a287641c5b7f
Roberto <142307266+itplutoplutone350@users.noreply.github.com>
2023-11-18 20:50:25 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -279,9 +279,8 @@ setTimeout(function() {
 
   const handleClickinfo =  () => {        
       // walletConnected è status variable 
-      // if (walletConnected.isSignedIn()) {
-        
-        alert ('This dApp is based on NEAR blockchain. Your messages will be stored inside the NEAR smart contract. Here you can add new messagges using your mynearwallet user. If you pay for premium then modify message feature will be enabled. For each message a dedicated web page is created for you .. you can share its link via socials or embed in your pages as iframe');
+      // if (walletConnected.isSignedIn())         
+      alert ('This dApp is based on NEAR blockchain. Your messages will be stored inside the NEAR smart contract. Here you can add new messagges using your MyNearwallet user. If you use the premium option then modify message feature will be enabled! For each message a dedicated web page is created for you .. you can share the link via socials or embed in your pages as iframe');
         }
   
   // definisco url del link al messaggio postato
